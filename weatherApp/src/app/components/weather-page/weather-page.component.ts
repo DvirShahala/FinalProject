6286c524a865931b05dcb8d4cdfc05381592159b
Dvir Shahala <P0024269@ness-tech.co.il>
2020-07-15 15:49:30 +0300

    cards with GIF

--- weatherApp/src/app/components/weather-page/weather-page.component.ts
+++ weatherApp/src/app/components/weather-page/weather-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { MomentPipe } from '../../pipes/moment/moment.pipe';
+// import { MomentPipe } from '../../pipes/moment/moment.pipe';
 import { Router } from '@angular/router';
+import * as moment from "moment";
+
 
 @Component({
   selector: 'app-weather-page',
@@ -13,8 +15,11 @@ export class WeatherPageComponent implements OnInit {
 
   private apiKey: string;
   public weatherSevenDays: any;
+  public sunriseSunset: any;
   public i: number = 0;
-  public dateWeather: MomentPipe;
+  public timeAMFM: string;
+  // public dateWeather: MomentPipe;
+  
 
   constructor(private http: HttpClient, private routes: Router) {
     this.apiKey = "b-Xq8MhhqNUWQT4016csTTQ2j--m8mksCwsnh8GfB-s";
@@ -25,6 +30,7 @@ export class WeatherPageComponent implements OnInit {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         this.getSevenDaysWeather(position.coords);
+        this.getsunriseSunset(position.coords);
       });
     } else {
       console.error("The browser does not support geolocation...");
@@ -45,6 +51,16 @@ export class WeatherPageComponent implements OnInit {
       });
   }
 
+  public getsunriseSunset(coordinates: any) {
+    this.http.jsonp("https://weather.ls.hereapi.com/weather/1.0/report.json?product=forecast_astronomy&latitude=" + coordinates.latitude + "&longitude=" + coordinates.longitude + "&apiKey=" + this.apiKey, "jsonpCallback")
+      .pipe(map(result => (<any>result).astronomy))
+      .subscribe(result => {
+        this.sunriseSunset = result.astronomy;
+      }, error => {
+        console.error(error);
+      });
+  }
+
   public getWindSpeed() {
     return this.weatherSevenDays[0].windSpeed;
   }
@@ -55,6 +71,15 @@ export class WeatherPageComponent implements OnInit {
 
   public getWindDesc() {
     return this.weatherSevenDays[0].beaufortDescription;
+  }
 
+  public getSunriseTime() {
+    this.timeAMFM = this.sunriseSunset[0].sunrise;
+    return moment(this.timeAMFM, ["h:mm A"]).format("HH:mm"); 
+  }
+  
+  public getSunsetTime() {
+    this.timeAMFM = this.sunriseSunset[0].sunset;
+    return moment(this.timeAMFM, ["h:mm A"]).format("HH:mm"); 
   }
 }
