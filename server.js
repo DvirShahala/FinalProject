6e9e8feae4da9fccad8aea127621718b7ef8695c
Dvir Shahala <P0024269@ness-tech.co.il>
2020-10-26 10:46:20 +0200

    change path in serve.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./weatherApp/dist/weather-app'));
+app.use(express.static('./weatherApp/dist/weatherApp'));
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'weatherApp/dist/weather-app' }
+    res.sendFile('index.html', { root: 'weatherApp/dist/weatherApp' }
     );
 });
 
